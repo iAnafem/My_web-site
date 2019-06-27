3cfddbebed128942366e292f3974e4ecf7a85e16
Пронькин Дмитрий Юрьевич <DPronkin@mostro.ru>
2019-06-27 16:53:12 +0300

    some changes in base.html

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['mysite/templates'],
+        'DIRS': ['mysite/templates/mysite'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
