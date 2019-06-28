3ae1cc59de993a2010eb9a06b8af293421c189a6
Пронькин Дмитрий Юрьевич <DPronkin@mostro.ru>
2019-06-28 14:20:19 +0300

    working on the home page. Create card-decks

--- mysite/homepage/models.py
+++ mysite/homepage/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class HomePage(models.Model):
     """Model representing the content on the home page from the other applications"""
     name = models.CharField(max_length=200, help_text="Name")
+    main_header = models.TextField(null=True)
+    secondary_header = models.TextField(null=True)
+    annotation = models.TextField(null=True)
     projects = models.ManyToManyField('portfolio.Project', related_name='projects')
 
     def __str__(self):
