4aa37452d64a4cca80b6eed2f6764a20dcf345b8
Пронькин Дмитрий Юрьевич <DPronkin@mostro.ru>
2019-07-16 10:50:56 +0300

    done with resume and contact pages

--- mysite/settings.py
+++ mysite/settings.py
@@ -177,15 +177,16 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mysite/static'),
-]
+
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'mysite/static'),
+# ]
 
 
-# STATIC_URL = '/static/'
-#
-# STATICFILES_DIRS = (os.path.join(CURRENT_PATH, 'static'),)
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(CURRENT_PATH, 'static'),)
 
 DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
